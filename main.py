d17137d145f0b56fed143a549f2d59ab5361a176
DanielGresak <80643624+DanielGresak@users.noreply.github.com>
2022-01-20 09:37:30 +0000

    adding random

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
+import random
 def main(text):
     print(text)
 
 
 if __name__ == '__main__':
     main("hello world!")
-    did_this_change = False
+    if random.randrange(0,2) == 1:
+        did_this_change = False
+    else: did_this_change = True
     if did_this_change:
         print("true")
         print("allgooddawg")
